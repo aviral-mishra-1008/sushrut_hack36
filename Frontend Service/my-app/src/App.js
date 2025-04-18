ad7a88a0f9a2628d2cfa4265b47dff956138a504
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2025-04-19 00:30:50 +0530

    Login signup done

--- Frontend Service/my-app/src/App.js
+++ Frontend Service/my-app/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+  return(
+     <div className="App">
+         <BrowserRouter>
+          <Routes>
+         <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+     </div>
+  )
+  
 }
 
 export default App;
